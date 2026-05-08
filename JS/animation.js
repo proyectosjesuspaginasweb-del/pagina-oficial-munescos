5c77c0bcf5b3069163024c4737e306161d8ef093
Jesus Cabrera <proyectosjesuspaginasweb@gmail.com>
2026-05-08 08:07:19 -0600

    se crea animacion de modal de catalogo

--- JS/animation.js
+++ JS/animation.js
@@ -1,3 +1,30 @@
+/*Efecto del nav al bajar*/
+
+const navAction = [
+	{id: "nav-action", className: "is-scrolled"},
+	{id: "nav-who-action", className: "is-scrolled-who"}
+]
+
+const elementosDOM = navAction.map(item => ({
+	el: document.getElementById(item.id),
+	className: item.className
+}));
+
+window.addEventListener("scroll", () =>{
+	const scrollNav = window.scrollY;
+	const isMobile = window.innerWidth <= 575;
+	
+	elementosDOM.forEach(({el, className}) =>{
+		if (!isMobile && scrollNav > 100) {
+			el.classList.add(className);
+		} else {
+			el.classList.remove(className);
+		}
+	});
+});
+
+
+
 /*Efecto de size de xo*/
 
 document.addEventListener("DOMContentLoaded", (event) => {
